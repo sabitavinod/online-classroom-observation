8bd7964cc4889d378424d0e76b164c5238f581ea
sabitavinod <129156178+sabitavinod@users.noreply.github.com>
2026-07-10 13:46:33 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.APP_CONFIG = {
+  // Replace after deploying the Apps Script as a Web App.
+  API_URL: 'https://script.google.com/macros/s/AKfycbxu7lm7hgl5dF1J6kizj1587-4XwQzWjdRQCp9BjVqCWhdXs7cQLUyP3hnIqzM49ge7/exec',
+
+  // Replace after creating a Web application OAuth Client ID in Google Cloud.
+  GOOGLE_CLIENT_ID: '1092459182340-73dd5mh8ufvgchq5f20ngj8i3ejgj17n.apps.googleusercontent.com',
+
+  ALLOWED_DOMAIN: 'mgsnagpur.org'
+};
